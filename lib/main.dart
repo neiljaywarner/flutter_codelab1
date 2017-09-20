5e1e53b02b3c9071cf7e9f86b969ee1ea11f5d45
Neil Jay Warner <njwandroid@gmail.com>
2017-09-19 19:43:31 -0500

    textfield, blue icon for accent theme, etc.

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,56 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
-  @override
+class ChatScreen extends StatefulWidget {                     //modified
+  @override                                                        //new
+  State createState() => new ChatScreenState();                    //new
+}
+
+// Add the ChatScreenState class definition in main.dart.
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController(); //new
+
+  @override                                                        //new
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Friendlychat")),
+      appBar: new AppBar(
+          title: new Text("Friendlychat")
+      ),
+        body: _buildTextComposer() //new
+
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(                                             //new
+        data: new IconThemeData(color: Theme.of(context).accentColor),  //new
+        child: new Container(                                      //modified
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Send a message"),
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text)),
+                ),
+              ]
+          ),
+        )                                                              //new
+    );
+  }
+
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
